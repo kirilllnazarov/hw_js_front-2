b8ee8d29263c2634f613b78d192c4ffefc5c6eaa
Kirill <kirill0392@gmail.com>
2024-12-27 10:29:04 +0300

    add 8th HW task

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,24 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: "sort"; payload: "up" | "down" } | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
-}
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
+	// need to fix any
+	switch (action.type) {
+        case "sort": {
+            if (action.payload === 'down' || action.payload === 'up') {
+                return [...state].sort((a, b) => 
+                    action.payload === 'up' 
+                        ? (a.name < b.name ? -1 : a.name > b.name ? 1 : 0) 
+                        : (a.name > b.name ? -1 : a.name < b.name ? 1 : 0) 
+                );
+            }
+			return state; // need to fix
+		}
+		case "check": {
+			return [...state].filter(s => s.age >= action.payload) // need to fix
+		}
+		default:
+			return state;
+	}
+};
