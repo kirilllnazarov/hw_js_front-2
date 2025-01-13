4ab6a01ff60f7d7f1a3404934782c7de86c7f857
Kirill <kirill0392@gmail.com>
2025-01-13 13:44:29 +0300

    add 10th HW

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,30 @@
-const initState = {
-    isLoading: false,
-}
+export type StateType = {
+	isLoading: boolean;
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+const initState: StateType = {
+	isLoading: false,
+};
 
-        default:
-            return state
-    }
-}
+export const loadingReducer = (state: StateType = initState, action: LoadingActionType): StateType => {
+	// fix any
+	switch (action.type) {
+		// пишет студент  // need to fix
+		case "CHANGE_LOADING": {
+			return { ...state, isLoading: action.isLoading };
+		}
+
+		default:
+			return state;
+	}
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+	type: "CHANGE_LOADING";
+	isLoading: boolean;
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+	type: "CHANGE_LOADING",
+	isLoading,
+});
