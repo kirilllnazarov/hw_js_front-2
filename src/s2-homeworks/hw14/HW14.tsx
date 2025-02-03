c8b547d28fbe66955804dd87e23410f692690221
Kirill <kirill0392@gmail.com>
2025-02-03 11:12:50 +0300

    add 13hw

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -9,9 +9,9 @@ import {useSearchParams} from 'react-router-dom'
 * 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
 * 2 - дописать функцию sendQuery в HW14
 * 3 - дописать функцию onChangeText в HW14
-* 4 - сделать стили в соответствии с дизайном
-* 5 - добавить HW14 в HW5/pages/JuniorPlus
-* */
+4 - сделать стили в соответствии с дизайном
+5 - добавить HW14 в HW5/pages/JuniorPlus
+*/
 
 const getTechs = (find: string) => {
     return axios
